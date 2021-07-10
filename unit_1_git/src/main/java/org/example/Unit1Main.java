7a45b2e6c730d792fce2e11dd7762a543063fba9
Iegor-Funtusov <funtushan@gmail.com>
2021-07-10 13:07:46 +0300

    add new class

--- unit_1_git/src/main/java/org/example/Unit1Main.java
+++ unit_1_git/src/main/java/org/example/Unit1Main.java
@@ -3,6 +3,8 @@ package org.example;
 public class Unit1Main {
 
     public static void main(String[] args) {
-        System.out.println("Unit1Main.main");
+        Student student = new Student();
+        student.setName("test");
+        System.out.println("student = " + student);
     }
 }
